8691ceef5156f187b9412a032b7f9c30b89fde11
jaider santiago <pjaidersantiago@gmail.com>
2025-07-18 12:30:41 -0500

    CRUD panel administrativo

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Member
+
+# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date", "email", "phone", "birth_date", "gender")
+  
+admin.site.register(Member, MemberAdmin)
 
 # Register your models here.
